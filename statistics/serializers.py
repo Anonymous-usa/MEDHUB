cfeb774ed0b55a3a2af247cde7ba9352e8e483cb
Buzurgmehr1 <bsftraders4@gmail.com>
2025-08-28 15:39:10 +0500

    Updated app statistics

--- statistics/serializers.py
+++ statistics/serializers.py
@@ -1,21 +1,29 @@
+# statistics/serializers.py
 from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 
+
 class OverviewStatsSerializer(serializers.Serializer):
-    total_institutions     = serializers.IntegerField()
-    total_hospitals        = serializers.IntegerField()
-    total_clinics          = serializers.IntegerField()
-    total_doctors          = serializers.IntegerField()
-    total_patients         = serializers.IntegerField()
-    total_requests         = serializers.IntegerField()
-    accepted_requests      = serializers.IntegerField()
-    rejected_requests      = serializers.IntegerField()
+    """
+    Обзорные показатели по всей системе.
+    """
+    total_institutions = serializers.IntegerField(label=_("Всего учреждений"))
+    total_hospitals = serializers.IntegerField(label=_("Всего больниц"))
+    total_clinics = serializers.IntegerField(label=_("Всего клиник"))
+    total_doctors = serializers.IntegerField(label=_("Всего врачей"))
+    total_patients = serializers.IntegerField(label=_("Всего пациентов"))
+    total_requests = serializers.IntegerField(label=_("Всего заявок"))
+    accepted_requests = serializers.IntegerField(label=_("Принятые заявки"))
+    rejected_requests = serializers.IntegerField(label=_("Отклонённые заявки"))
 
 
 class InstitutionStatsSerializer(serializers.Serializer):
-    institution_id         = serializers.IntegerField()
-    name                   = serializers.CharField()
-    total_doctors          = serializers.IntegerField()
-    total_requests         = serializers.IntegerField()
-    accepted_requests      = serializers.IntegerField()
-    rejected_requests      = serializers.IntegerField()
+    """
+    Показатели по конкретному учреждению.
+    """
+    institution_id = serializers.IntegerField(label=_("ID учреждения"))
+    name = serializers.CharField(label=_("Название учреждения"))
+    total_doctors = serializers.IntegerField(label=_("Всего врачей"))
+    total_requests = serializers.IntegerField(label=_("Всего заявок"))
+    accepted_requests = serializers.IntegerField(label=_("Принятые заявки"))
+    rejected_requests = serializers.IntegerField(label=_("Отклонённые заявки"))
